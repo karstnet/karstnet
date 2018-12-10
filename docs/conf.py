e63c886ae30506390d9a402312abb491c81312bb
philipperenard <philippe.renard@unine.ch>
2018-12-10 17:13:35 +0100

    Bug fix travis configuration

--- docs/conf.py
+++ docs/conf.py
@@ -39,10 +39,10 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.imgmath',
-    'numpydoc']
+              'sphinx.ext.doctest',
+              'sphinx.ext.todo',
+              'sphinx.ext.imgmath',
+              'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
